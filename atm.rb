fcaa89348894192b9a56ddd20b1f43f77855064b
Studentenblume <39347774+Studentenblume@users.noreply.github.com>
2018-05-19 21:40:50 +0300

    Update atm.rb

--- atm.rb
+++ atm.rb
@@ -2,15 +2,53 @@
 require 'yaml'
 info = YAML.load(File.open(File.join(File.dirname(_FILE_), config.yml)))
 
-print "Please Enter Your Account Number:"
-acNum = gets
-print "Enter your password:"
+class BankAccount
+
+attr_accessor :name, :password, :accaunt_num, :balance
+
+def initialize
+    @your_account = []
+    open()
+end
+
+def open
+    if File.exist?("config.yml")
+    @your_account = YAML.load_file("config.yml")
+    end
+end
+
+ def account_review(your_account)
+    puts @your_acccount
+end
+
+def run
+    loop do
+        puts "Welcome to the bank."
+        puts "1. Create New Account"
+        puts "2. Review Your Account Information"
+        puts "3. Check Your Balance"
+        puts "4. Exit"
+        puts "Enter your choice:"
+            input = gets.chomp
+            case input
+            when '1'
+                new_account(first_name, last_name, address)
+            when '2'
+                account_review(your_account)
+            when '4'
+                save()
+                break
+            end
+    end
+end
+
+end
 
 #user = User.find_for_authentication(username: params[:username])
 #user.valid_password?(params[:password])
 
 def login
-  user = User.find_for_authentication(email: login_params[:email])
+  user = User.find_for_authentication(acc_num: login_params[:accaunt_num])
 
   if user.valid_password?(login_params[:password])
     user.remember_me = login_params[:remember_me]
@@ -24,7 +62,7 @@ def login_params
 end
 
 module Run
-include #файл входа
+include initialize
     loop do 
     puts"1) Display Balance"
     puts"2) Withdraw"
@@ -48,7 +86,8 @@ include #файл входа
               summ = gets
               if sum =< balace
               balance += summ.to_f
-               puts"Your New Balance is"+ balance
+               puts"Your New Balance is"+ :balance
+                balance = balance.new
                 else 
                     puts "ERROR: INSUFFICIENT FUNDS!! PLEASE ENTER A DIFFERENT AMOUNT:"
                     summ = gets
@@ -65,15 +104,4 @@ include #файл входа
             
 
                 
-                puts"\n"
-                puts "What do you want to do today?"
-                puts"-----------------------------------"
-                puts"1)Deposit  2)Withdrawal 3)Transfer 4)Balance 5)Exit"
-                print"your choice :"
-                ans = gets.chomp.to_i
-                case ans 
-                
-                end 
-
-            end
-       
+              
