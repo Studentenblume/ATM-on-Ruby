a05a14f9576b58cd4636621bd21c9ef0ec37ae72
Studentenblume <39347774+Studentenblume@users.noreply.github.com>
2018-05-24 16:46:45 +0300

    Update atm.rb

--- atm.rb
+++ atm.rb
@@ -1,40 +1,22 @@
 
 require 'yaml'
 
-class person
-info :name, :password, :accaunt_num, :balance
-end
+got_data = YAML.load(File.open(File.join(File.dirname(_FILE_), 'config.yaml')))
 
-def initialize
-    @your_account = []
-    open()
-end
-
-def open
-    if File.exist?("config.yml")
-    @your_account = YAML.load_file("config.yml")
-    else 
-        print "ACCOUNT NUMBER AND PASSWORD DON'T MATCH"
-    end
-end
-
-module Info
-    loop do
-        puts "Please Enter Your Account Number:"
+def inizialaze
+    puts "Please Enter Your Account Number:"
         num = gets.to_i
-        puts "Enter Your Password:"
+    puts "Enter Your Password:"
         pass = gets.to_i
-            if num && pass = :accaunt_num, :password
-               puts "Hello, " + :name
+            if num && pass = [:accaunt_num] && [:password]
+               puts "Hello, " + [:name]
             else
                 puts "ERROR: ACCOUNT NUMBER AND PASSWORD DON'T MATCH"
                 break
             end
     end
-                
-module Run
-include Info
-    loop do 
+            
+def menu
     puts"1) Display Balance"
     puts"2) Withdraw"
     puts"3) Log Out"
@@ -49,23 +31,23 @@ include Info
 
         case ans
         when 1
-            puts"Your Current Balance is"+ :balance
+            puts"Your Current Balance is"+ [:balance]
                       
             end
          when 2
             puts"Enter Amount You Wish to Withdraw:" 
               summ = gets
-              if summ =< :balace
+              if summ =< [:balace]
                 balance -= summ.to_f
                 puts"Your New Balance is"+ :balance
                 balance = balance.new
-                    if summ = :balance
+                    if summ = [:balance]
                         puts "ERROR: THE AMOUNT YOU REQUESTED CANNOT BE COMPOSED FROM BILLS AVAILABLE IN THIS ATM. PLEASE ENTER A DIFFERENT AMOUNT:"
                         summ = gets
                                      
                 end
           when 3
-          puts :name + "Thank You For Using Our ATM. Good-Bye!"
+          puts [:name] + "Thank You For Using Our ATM. Good-Bye!"
           break
           end
     
